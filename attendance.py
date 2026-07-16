5a15431fc5ac285bbefc6f1e2c21d388262a7e33
shikha Yadav <shikhayadavhasnapur@gmail.com>
2026-07-16 11:22:08 +0530

    Added attendance tracking

--- attendance.py
+++ attendance.py
@@ -1,6 +1,9 @@
 attendance_records = {}
 def mark_attendance():
+    
     name = input("Enter student name:")
     status = input("Present or Absent(P/A): ")
     attendance_records[name] = status.upper()
+    with open("attendance.txt", "a") as file:
+        file.write(f"{name},{status}:\n")
     print("Attendance marked successfully.")
